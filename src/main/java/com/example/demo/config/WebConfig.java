e4ce437b2a7a50961498bb354a9f922159bea905
xxIrynaxx <irakalachenko2006@gmail.com>
2025-07-23 12:36:38 +0300

    update config

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:2025")
+            .allowedOrigins("https://ryu-no-kinu.netlify.app/")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
